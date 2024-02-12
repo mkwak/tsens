3d0cc70eff17b7846a52aab2b200faa2f3d9151b
Minseok Kwak <minseok.kwak@gmail.com>
2024-02-12 23:28:02 +0900

    Update mysql_settings.php

--- web/mysql_settings.php
+++ web/mysql_settings.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = 'YOUR_HOSTNAME';  //MySQL hostname
-$mysql_username = 'MYSQL_USER';  //MySQL username
-$mysql_password = 'MYSQL_PASSWD';  //MySQL user password
-$mysql_database = 'MYSQL_DB'; //MySQL database
-$mysql_port = '3306'; //Default MySQL port
-$mysql_charset = 'utf8';  //encoding
+$mysql_username = 'MYSQL_USER';     //MySQL username
+$mysql_password = 'MYSQL_PASSWD';   //MySQL user password
+$mysql_database = 'MYSQL_DB';       //MySQL database
+$mysql_port = '3306';               //Default MySQL port
+$mysql_charset = 'utf8';            //encoding
 ?>
